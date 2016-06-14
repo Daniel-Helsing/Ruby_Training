e01059c1950dff658e4c9d9f4bea64e63948e5a8
Daniel Helsing <danielhelsing3@gmail.com>
2016-06-14 14:44:35 -0400

    Final Ch.4 file

--- ruby1.rb
+++ ruby1.rb
@@ -3,3 +3,11 @@ array = [1,2,3,4,5]
 
 array2 = [3,1,5,2,4]
 array2.sort{|v1,v2| v1 <=> v2 }
+
+fruits = ['banana', 'apple'. 'orange','pear']
+fruits.sort
+fruits.sort_by{|fruit| fruit.length}
+
+array3 = [*1..10]
+sum= array3.inject {|memo, n| memo +n}
+sum= array3.inject(100){|memo, n| memo+n}
