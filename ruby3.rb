a904f83f1174f59bd81b64b0a35e9c16871bc735
Daniel Helsing <danielhelsing3@gmail.com>
2016-06-20 12:01:50 -0400

    Attribute methods

--- ruby3.rb
+++ ruby3.rb
@@ -1,15 +1,33 @@
 class Animal
-  def set_noise(noise)
+  attr_accessor :name
+  attr_writer :color
+  attr_reader :legs, :arms
+  def setup_limbs
+  		@legs = 4
+  		@arms = 0
+  end	
+  def noise=(noise)
   	@noise = noise
-  def make_noise
+  end
+  def noise
     @noise
+  end
+  def color
+  	"The color is #{@color}"
   end
 end
 
 animal = Animal.new
-animal.set_noise("Moo!")
-puts animal.make_noise
+animal.setup_limbs
+animal.noise = "Moo!"
+animal.name = "Steve"
+puts animal.name
+animal.color = "black"
+puts animal.color
+puts animal.legs
+puts animal.noise
+
 
 animal2 = Animal.new
-animal2.set_noise("Oink!")
-puts animal2.make_noise
+animal2.noise = "Oink!"
+puts animal2.noise
