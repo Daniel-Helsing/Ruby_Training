1bbedd801abe005ade54db48486bbbf11618b4af
Daniel Helsing <danielhelsing3@gmail.com>
2016-06-26 21:50:02 -0400

    Balls hut

--- lib/guide.rb
+++ lib/guide.rb
@@ -1,4 +1,8 @@
 class Guide
+  class Config
+      @@actions = ['list', 'find', 'add', 'quit']
+      def self.actions; @@actions; end
+  end
 
 	def initialize(path=nil)
 		Restaurant.filepath = path
@@ -16,11 +20,21 @@ class Guide
 		introduction
     result = nil
     until result == :quit
-      print "> "
-      user_response = gets.chomp
-      result = do_action(user_response)
+      action = get_action
+      result = do_action(action)
     end
       conclusion
+  end
+
+  def get_action
+    action = nil
+    until Guide::Config.actions.include?(action)
+      puts "Actions: " + Guide::Config.actions.join(", ") if action
+      print "> "
+      user_response = gets.chomp
+      action = user_response.downcase.strip
+    end
+    return action
   end
 
   def do_action(action)
@@ -30,11 +44,29 @@ class Guide
     when 'find'
       puts "Fining"
     when 'add'
-      puts "Adding..."
+      add
     when 'quit'
       return :quit
     else 
       puts "\nI don't understand that command.\n"
+    end
+  end
+
+  def add
+    puts "\nAdd a restaurant\n\n".upcase
+    restaurant = Restaurant.new
+
+    print "Restaurant name: "
+    restaurant.name = gets.chomp.strip
+    print "Cuisine type: "
+    restaurant.cuisine = gets.chomp.strip
+    print "Average price: "
+    restaurant.price = gets.chomp.strip
+    
+    if restaurant.save
+      puts "\nRestaurant Added\n\n"
+    else
+      puts "\nError: restaurant not added\n\n"
     end
   end
 
--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -4,7 +4,7 @@ class Restaurant
   def self.filepath=(path=nil)
     @@filepath = File.join(APP_ROOT, path)
   end
-
+  attr_accessor :name, :cuisine, :price
   def self.file_exists?
     if @@filepath && File.exists?(@@filepath)
       return true
@@ -31,6 +31,13 @@ class Restaurant
   def self.saved_restaurants
     # read the restaurant file
     # return instances of restaurant
+  end
+  def save
+    return false unless Restaurant.file_usable?
+    File.open(@@filepath, 'a') do |file|
+      file.puts "#{[@name, @cuisine, @price].join("\t")}\n"
+    end
+    return true
   end
 
 end
