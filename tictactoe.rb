24ce43e1b5d9ee2843eee6106436be8768004aa7
Daniel Helsing <danielhelsing3@gmail.com>
2016-06-30 12:34:11 -0400

    Tic tac toe update

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,17 +1,19 @@
 class Tictactoe
-@@board = []
+@@board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 @@play_on = true
+@@counter = 0
 game= Tictactoe.new
 
 
 def play
   while @@play_on == true
-    puts "What box number would you like to select"
-    number = gets.chomp
-    print "Please select x or o"
-    selection = gets.chomp.downcase
-    number --
-    @@board[number] = selection
+    puts "What box number would you like to select\n"
+    x = gets
+    print "Please select x or o\n"
+    selection = gets.chomp.downcase.strip
+    x --
+    @@board[x] = selection
+    @@counter++
     game.check
   end
   puts "Thank you for playing"
@@ -50,6 +52,8 @@ def check
     @@play_on == false end
   if (@@board[2] == "o" && @@board[4] == "o" && @@board[6] == "o") 
     @@play_on == false end
+  if @@counter == 9
+    @@play_on == false end  
 end
 game.play
 end
