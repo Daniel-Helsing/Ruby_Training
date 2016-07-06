fc9054780bcb0e030fc05da83e9bfdeee764c7cb
Daniel Helsing <danielhelsing3@gmail.com>
2016-07-06 15:03:26 -0400

    Tic tac toe big bro

--- tictactoe.rb
+++ tictactoe.rb
@@ -4,56 +4,53 @@
 
 def check
   if (@board[0] == "x" && @board[1] == "x" && @board[2] == "x") 
-    @play_on = false 
+    @play_on = false
   end
   if (@board[3] == "x" && @board[4] == "x" && @board[5] == "x") 
-    @play_on = false 
+    @play_on = false
   end
   if (@board[6] == "x" && @board[7] == "x" && @board[8] == "x") 
-    @play_on = false 
+    @play_on = false
   end
   if (@board[0] == "x" && @board[3] == "x" && @board[6] == "x") 
-    @play_on = false 
+    @play_on = false
   end
   if (@board[1] == "x" && @board[4] == "x" && @board[7] == "x") 
-    @play_on = false 
+    @play_on = false  
   end
-  if (@board[2] = "x" && @board[5] == "x" && @board[8] == "x") 
-    @play_on = false 
+  if (@board[2] == "x" && @board[5] == "x" && @board[8] == "x") 
+    @play_on = false
   end
   if (@board[0] == "x" && @board[4] == "x" && @board[8] == "x") 
-    @play_on = false 
+    @play_on = false
   end
   if (@board[2] == "x" && @board[4] == "x" && @board[6] == "x") 
-    @play_on = false 
+    @play_on = false  
   end
   if (@board[0] == "o" && @board[1] == "o" && @board[2] == "o") 
     @play_on = false 
   end
   if (@board[3] == "o" && @board[4] == "o" && @board[5] == "o") 
-    @play_on = false 
+    @play_on = false  
   end
   if (@board[6] == "o" && @board[7] == "o" && @board[8] == "o") 
-    @play_on = false 
+    @play_on = false   
   end
   if (@board[0] == "o" && @board[3] == "o" && @board[6] == "o") 
     @play_on = false 
   end
-  if (@board[1] = "o" && @board[4] == "o" && @board[7] == "o") 
-    @play_on = false 
+  if (@board[1] == "o" && @board[4] == "o" && @board[7] == "o") 
+    @play_on = false
   end
   if (@board[2] == "o" && @board[5] == "o" && @board[8] == "o") 
-    @play_on = false 
+    @play_on = false
   end
   if (@board[0] == "o" && @board[4] == "o" && @board[8] == "o") 
-    @play_on = false 
+    @play_on = false  
   end
   if (@board[2] == "o" && @board[4] == "o" && @board[6] == "o") 
-    @play_on = false 
+    @play_on = false
   end
-  if @counter == 9
-    @play_on = false 
-  end  
 end
 
 def output_tictactoe
@@ -65,17 +62,24 @@ def output_tictactoe
 end
 
 def play_tic
-  until @play_on == false
+  until @play_on == false || @counter == 9
     output_tictactoe
     puts "Player 1:(x) What box number would you like to select"
     x = gets.strip.to_i
     @board[x] = "x"
-    output_tictactoe
-    puts "Player 2:(o) What box number would you like to select\n"
-    o = gets.strip.to_i
-    @board[o] = "o"
+    check
+    @counter+=1
+    if(@counter!=9 && @play_on == true)
+      output_tictactoe
+      puts "Player 2:(o) What box number would you like to select\n"
+      o = gets.strip.to_i
+      @board[o] = "o"
+      check
+      @counter+=1
+    end
   end
   puts "Thank you for playing"
+  output_tictactoe
 end
 
 play_tic
