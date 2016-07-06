e2ac90f9c4f07e2d585492db8b769102443eec40
Daniel Helsing <danielhelsing3@gmail.com>
2016-07-06 14:08:56 -0400

    getting closer

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,80 +1,85 @@
-class Tictactoe
-@board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+@@board = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
 @play_on = true
 @counter = 0
-game= Tictactoe.new
+play_tic
+def output_tictactoe
+  puts @@board[0]+"|"+@@board[1]+"|"+@@board[2]
+  puts "-----"
+  puts @@board[3]+"|"+@@board[4]+"|"+@@board[5]
+  puts "-----"
+  puts @@board[6]+"|"+@@board[7]+"|"+@@board[8]
+end
 
-
-def play
+def play_tic
   until @play_on == false
-    do
-    puts "Player 1:(x) What box number would you like to select\n"
-    x = gets.chomp
-    x --
-    @board[x] = "x"
+    output_tictactoe
+    puts "Player 1:(x) What box number would you like to select"
+    x = gets.strip.to_i
+    @@board[x] = "x"
     @counter++
+    check
+    output_tictactoe
     puts "Player 2:(o) What box number would you like to select\n"
-    o = gets.chomp
-    o --
-    @board[o] = "o"
+    o = gets.strip.to_i
+    @@board[o] = "o"
     @counter++
-    game.check
+    check
   end
   puts "Thank you for playing"
 end
 
+
 def check
-  if (@board[0] == "x" && @board[1] == "x" && @board[2] == "x") 
+  if (@@board[0] == "x" && @@board[1] == "x" && @@board[2] == "x") 
     @play_on == false 
   end
-  if (@board[3] == "x" && @board[4] == "x" && @board[5] == "x") 
+  if (@@board[3] == "x" && @@board[4] == "x" && @@board[5] == "x") 
     @play_on == false 
   end
-  if (@board[6] == "x" && @board[7] == "x" && @board[8] == "x") 
+  if (@@board[6] == "x" && @@board[7] == "x" && @@board[8] == "x") 
     @play_on == false 
   end
-  if (@board[0] == "x" && @board[3] == "x" && @board[6] == "x") 
+  if (@@board[0] == "x" && @@board[3] == "x" && @@board[6] == "x") 
     @play_on == false 
   end
-  if (@board[1] == "x" && @board[4] == "x" && @board[7] == "x") 
+  if (@@board[1] == "x" && @@board[4] == "x" && @@board[7] == "x") 
     @play_on == false 
   end
-  if (@board[2] == "x" && @board[5] == "x" && @board[8] == "x") 
+  if (@@board[2] == "x" && @@board[5] == "x" && @@board[8] == "x") 
     @play_on == false 
   end
-  if (@board[0] == "x" && @board[4] == "x" && @board[8] == "x") 
+  if (@@board[0] == "x" && @@board[4] == "x" && @@board[8] == "x") 
     @play_on == false 
   end
-  if (@board[2] == "x" && @board[4] == "x" && @board[6] == "x") 
+  if (@@board[2] == "x" && @@board[4] == "x" && @@board[6] == "x") 
     @play_on == false 
   end
-  if (@board[0] == "o" && @board[1] == "o" && @board[2] == "o") 
+  if (@@board[0] == "o" && @@board[1] == "o" && @@board[2] == "o") 
     @play_on == false 
   end
-  if (@board[3] == "o" && @board[4] == "o" && @board[5] == "o") 
+  if (@@board[3] == "o" && @@board[4] == "o" && @@board[5] == "o") 
     @play_on == false 
   end
-  if (@board[6] == "o" && @board[7] == "o" && @board[8] == "o") 
+  if (@@board[6] == "o" && @@board[7] == "o" && @@board[8] == "o") 
     @play_on == false 
   end
-  if (@board[0] == "o" && @board[3] == "o" && @board[6] == "o") 
+  if (@@board[0] == "o" && @@board[3] == "o" && @@board[6] == "o") 
     @play_on == false 
   end
-  if (@board[1] == "o" && @board[4] == "o" && @board[7] == "o") 
+  if (@@board[1] == "o" && @@board[4] == "o" && @@board[7] == "o") 
     @play_on == false 
   end
-  if (@board[2] == "o" && @board[5] == "o" && @board[8] == "o") 
+  if (@@board[2] == "o" && @@board[5] == "o" && @@board[8] == "o") 
     @play_on == false 
   end
-  if (@board[0] == "o" && @board[4] == "o" && @board[8] == "o") 
+  if (@@board[0] == "o" && @@board[4] == "o" && @@board[8] == "o") 
     @play_on == false 
   end
-  if (@board[2] == "o" && @board[4] == "o" && @board[6] == "o") 
+  if (@@board[2] == "o" && @@board[4] == "o" && @@board[6] == "o") 
     @play_on == false 
   end
   if @counter == 9
     @play_on == false 
   end  
-end
-game.play
 end
